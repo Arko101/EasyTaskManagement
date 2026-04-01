098bb2c5cbad87527d489a64c8533c392fcbae60
Arkopratim Chakraborty <arkoribhu@gmail.com>
2026-04-01 20:29:29 +0530

    Made users an interative buttonand added images to each user

--- attachments/02-essentials/01-starting-project/src/app/app.component.ts
+++ attachments/02-essentials/01-starting-project/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
