2b1929be67da1f4dc76b388df47896fd0d5defdf
Arkopratim Chakraborty <arkoribhu@gmail.com>
2026-04-16 00:32:29 +0530

    Added asnw task component for usrer-specific tasks

--- attachments/02-essentials/01-starting-project/src/app/tasks/tasks.component.ts
+++ attachments/02-essentials/01-starting-project/src/app/tasks/tasks.component.ts
@@ -1,25 +1,28 @@
 import { Component, EventEmitter, Input , input, Output } from '@angular/core';
+//import { TaskComponent } from './task/task.component';
+import { TaskComponent } from './task/task.component';
 
 
 
 @Component({
-  selector: 'app-task',
+  selector: 'app-tasks',
   standalone: true,
   imports: [ TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
-export class TaskComponent {
+export class TasksComponent {
 
 //@Input({required: true}) selectedUserId!: string;
 @Input() name?: string;
 
+
 }
 
+//  onSelectUser() {
 
-// onSelectUser() {
+//    this.select.emit(this.name);;
 
-//   this.select.emit(this.name);;
-
+//  }
 // }
-// }
+
