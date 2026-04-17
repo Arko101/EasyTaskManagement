f87f6566dbf3d0e795b196a4ba0955ec975aedc7
Chakraborty, Arkopratim (FT D AA IN PAMC DCP PNI-CP-IDENT) <arkopratim.chakraborty@siemens.com>
2026-04-18 00:45:40 +0530

    Added task complete feature

--- attachments/02-essentials/01-starting-project/src/app/tasks/task/task.component.ts
+++ attachments/02-essentials/01-starting-project/src/app/tasks/task/task.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+interface Task {
+  id: string;
+  userId: string;
+  title: string;
+  summary: string;
+  dueDate: string;
+}
+
 
 @Component({
   selector: 'app-task',
@@ -8,5 +17,15 @@ import { Component } from '@angular/core';
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
+  @Input({required: true}) task!: Task;
+  @Output() complete = new EventEmitter<string>();
+
+
+  onCompleteTask() {
+    this.complete.emit(this.task.id);
+  }
+  
+
+
 
 }
--- attachments/02-essentials/01-starting-project/src/app/tasks/tasks.component.ts
+++ attachments/02-essentials/01-starting-project/src/app/tasks/tasks.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Input , input, Output } from '@angular/core';
-//import { TaskComponent } from './task/task.component';
 import { TaskComponent } from './task/task.component';
 
 
@@ -13,12 +12,47 @@ import { TaskComponent } from './task/task.component';
 })
 export class TasksComponent {
 
-//@Input({required: true}) selectedUserId!: string;
-@Input() name?: string;
+@Input({required: true}) selectedUserId!: string;
+@Input({required: true}) name?: string;
 
 
+
+dummyTasks = [
+  {
+    id: 't1',
+    userId: 'u1',
+    title: 'Master Angular',
+    summary:
+      'Learn all the basic and advanced features of Angular & how to apply them.',
+    dueDate: '2025-12-31',
+  },
+  {
+    id: 't2',
+    userId: 'u3',
+    title: 'Build first prototype',
+    summary: 'Build a first prototype of the online shop website',
+    dueDate: '2024-05-31',
+  },
+  {
+    id: 't3',
+    userId: 'u3',
+    title: 'Prepare issue template',
+    summary:
+      'Prepare and describe an issue template which will help with project management',
+    dueDate: '2024-06-15',
+  },
+]
+
+
+
+get assignedTasks() {
+  return this.dummyTasks.filter(task => task.userId === this.selectedUserId);
 }
 
+onCompleteTask(id: string) {
+  this.dummyTasks = this.dummyTasks.filter((task) => task.id !== id);
+}
+}
 //  onSelectUser() {
 
 //    this.select.emit(this.name);;
